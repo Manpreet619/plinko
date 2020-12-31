bd61f55c7a206e8a0f7c6de3fd15d39bfd8c1031
Manpreet619 <71409313+Manpreet619@users.noreply.github.com>
2020-12-31 18:03:14 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  var particals 
+  var plinko
+  var Divistions 
+  var Ground 
+  
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
